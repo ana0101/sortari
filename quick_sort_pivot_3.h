f2bc3a7849ff1dcae349a1e19013ef53452777c5
ana <anamaria.hodivoianu@gmail.com>
2023-03-15 19:50:15 +0200

    made quicksort good for duplicates

--- quick_sort_pivot_3.h
+++ quick_sort_pivot_3.h
@@ -12,23 +12,15 @@ int mediana(int e1, int e2, int e3)
 }
 
 
-void quickSort2(int v[], int st, int dr)
+void quickSort2(int v[], int inc, int sf)
 {
-    if(st < dr)
+    if(inc < sf)
     {
-        int pivot = mediana(v[st], v[(st+dr)/2], v[dr-1]);
-        int poz_pivot;
-        if(pivot == v[st])
-            poz_pivot = st;
-        else
-        {
-            if(pivot == v[(st+dr)/2])
-                poz_pivot = (st+dr)/2;
-            else
-                poz_pivot = dr-1;
-        }
-        poz_pivot = ordonare(v, st, dr, poz_pivot);
-        quickSort2(v, st, poz_pivot);
-        quickSort2(v, poz_pivot+1, dr);
+        int pivot = mediana(v[inc], v[(inc+sf)/2], v[sf]);
+        int poz[3];
+        ordonare(v, inc, sf, pivot, poz);
+        int lim1 = poz[0], lim2 = poz[1];
+        quickSort2(v, inc, lim1-1);
+        quickSort2(v, lim2, sf);
     }
 }
--- quick_sort_pivot_random.h
+++ quick_sort_pivot_random.h
@@ -1,39 +1,44 @@
-int ordonare(int v[], int st, int dr, int poz_pivot)
+void ordonare(int v[], int inc, int sf, int pivot, int poz[])
 {
-    int j = 0;
-    for(int i=st; i<dr; i++)
-        if(v[i] <= v[poz_pivot] && i != poz_pivot)
+    int lim1 = inc, lim2 = inc, lim3 = sf;
+    //inc - lim1: < pivot
+    //lim1 - lim2: == pivot
+    //lim2 - lim3: neprocesate inca
+    //lim3 - sf: > pivot
+    while(lim2 <= lim3)
+    {
+        if(v[lim2] < pivot)
         {
-            aux[j] = v[i];
-            j ++;
+            std::swap(v[lim1], v[lim2]);
+            lim1 ++;
+            lim2 ++;
         }
-
-    aux[j] = v[poz_pivot];
-    int poz_pivot2 = j + st;
-    j ++;
-    
-    for(int i=st; i<dr; i++)
-        if(v[i] > v[poz_pivot])
+        else
         {
-            aux[j] = v[i];
-            j ++;
+            if(v[lim2] > pivot)
+            {
+                std::swap(v[lim2], v[lim3]);
+                lim3 --;
+            }
+            else
+                lim2 ++;
         }
-
-    for(int i=st; i<dr; i++)
-        v[i] = aux[i-st];
-
-    return poz_pivot2;
+    }
+    poz[0] = lim1, poz[1] = lim2;
 }
 
 
-void quickSort1(int v[], int st, int dr)
+void quickSort1(int v[], int inc, int sf)
 {
-    if(st < dr)
+    if(inc < sf)
     {
         srand(time(0));
-        int poz_pivot = st + rand() % (dr-st);
-        poz_pivot = ordonare(v, st, dr, poz_pivot);
-        quickSort1(v, st, poz_pivot);
-        quickSort1(v, poz_pivot+1, dr);
+        int poz_pivot = inc + rand() % (sf-inc);
+        int pivot = v[poz_pivot];
+        int poz[3];
+        ordonare(v, inc, sf, pivot, poz);
+        int lim1 = poz[0], lim2 = poz[1];
+        quickSort1(v, inc, lim1-1);
+        quickSort1(v, lim2, sf);
     }
 }
